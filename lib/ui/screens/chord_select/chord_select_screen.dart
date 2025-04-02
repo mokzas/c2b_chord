6afb6473a468404d8277ef2b779868ddf683f670
IKJ-DESKTOP\JaeikLee <jaeiklee.dev@gmail.com>
2025-04-03 00:25:17 +0900

    Feat: Preset 탭 숨김 (#16)
    - Preset 기능은 추후 버전에 포함하는 것으로 변경

--- lib/ui/screens/chord_select/chord_select_screen.dart
+++ lib/ui/screens/chord_select/chord_select_screen.dart
@@ -35,10 +35,10 @@ class _ChordSelectScreenState extends ConsumerState<ChordSelectScreen> {
             icon: Icon(Icons.check),
             label: Text('Select'),
           ),
-          NavigationRailDestination(
-            icon: Icon(Icons.bookmark_outline),
-            label: Text('Preset'),
-          ),
+          // NavigationRailDestination(
+          //   icon: Icon(Icons.bookmark_outline),
+          //   label: Text('Preset'),
+          // ),
         ],
         selectedIndex: _selectType.index,
         onDestinationSelected: (index) {
