24ed373c7b41bc8bf6158594ceb2e9682eb63fc8
cerik <cerik.dev@gmail.com>
2025-04-26 01:36:24 +0900

    Chore: debug configuration 추가 #33
    - debug config cnrk
    - release build에 shrinking, obfuscation, and optimization 옵션 추가
    - 빌드 테스트를 위해 잠시 main.yml 파일 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -44,19 +44,26 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
             storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
+            storePassword = keystoreProperties["storePassword"] as String?
         }
     }
 
     buildTypes {
+        debug {
+            isDebuggable = true
+            signingConfig = signingConfigs.getByName("debug")
+        }
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
+            isMinifyEnabled = true
+            isShrinkResources = true
             signingConfig = signingConfigs.getByName("release")
         }
+
     }
 }
 
