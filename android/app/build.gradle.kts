0cb8182082566df433e1938c5c95fd23b9f67423
ikj <ikj@ikjs-MacBook-Pro.local>
2026-08-27 21:13:06 +0900

    Chore: Google Play API 36 release 준비

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,16 +19,16 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "kr.mokzas.c2b_chord"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "27.0.12077973"//flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -37,7 +37,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
